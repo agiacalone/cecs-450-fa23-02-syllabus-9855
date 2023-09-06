ead8918f03ff9aa4fe217936958f312ab597e95b
Anthony Giacalone <anthony.giacalone@csulb.edu>
2023-09-06 15:37:26 -0700

    fixed pdf output naming scheme for admin

--- gen_output.sh
+++ gen_output.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pandoc README.md -o cecs-450-fa23-02-syllabus-9855.html --from markdown
-pandoc README.md --template eisvogel -V linkcolor=blue -V header-includes:'\usepackage[export]{adjustbox} \let\includegraphicsbak\includegraphics \renewcommand*{\includegraphics}[2][]{\includegraphicsbak[frame,#1]{#2}}' -o cecs-450-fa23-02-syllabus-9855.pdf
+pandoc README.md --template eisvogel -V linkcolor=blue -V header-includes:'\usepackage[export]{adjustbox} \let\includegraphicsbak\includegraphics \renewcommand*{\includegraphics}[2][]{\includegraphicsbak[frame,#1]{#2}}' -o 'CECS 450_02_FA23_Giacalone_Anthony.pdf'
 
